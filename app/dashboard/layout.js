f5999d04cce6f1f3fde921e7fde01699aa7be62e
Aditio <aditio.pluto@gmail.com>
2024-10-21 08:11:43 +0700

    Update layout

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -1,11 +1,27 @@
 "use client";
 
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 
 const { Navbar, NavbarBrand, NavbarContent, NavbarItem } = require("@nextui-org/react");
 const { default: Link } = require("next/link");
 
 const DashboardLayout = () => {
+    const navbarItems = [
+        {
+            name: 'Dashboard',
+            route: '/dashboard',
+        },
+        {
+            name: 'Product Category',
+            route: 'product-category',
+        },
+        {
+            name: 'Product',
+            route: '/dashboard/product',
+        }
+    ];
+
     return (
         <div>
             <Navbar position="static">
@@ -13,17 +29,15 @@ const DashboardLayout = () => {
                     <Image src="/gree-logo.svg" className="mb-3" alt="Gree Logo" width={100} height={100}></Image>
                 </NavbarBrand>
                 <NavbarContent className="hidden sm:flex gap-4" justify="center">
-                    <NavbarItem className="hidden sm:flex gap-4">
-                        <Link color="foreground" href="/dashboard">
-                            Dashboard
-                        </Link>
-                        <Link color="foreground" href="/dashboard">
-                            Product Category
-                        </Link>
-                        <Link color="foreground" href="/dashboard">
-                            Product
-                        </Link>
-                    </NavbarItem>
+                    {
+                        (navbarItems.map((item) => {
+                            return <NavbarItem isActive={usePathname() == item.route}>
+                                <Link className="hover:text-blue-700" href={item.route}>
+                                    {item.name}
+                                </Link>
+                            </NavbarItem>
+                        }))
+                    }
                 </NavbarContent>
                 <NavbarContent justify="end">
 
