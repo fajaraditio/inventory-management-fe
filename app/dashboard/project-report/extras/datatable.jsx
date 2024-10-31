81ffb756d551d536412ed979f4f69f05f960fc3e
Aditio <aditio.pluto@gmail.com>
2024-10-31 16:56:48 +0700

    Update datatable project service

--- app/dashboard/project-report/extras/datatable.jsx
+++ app/dashboard/project-report/extras/datatable.jsx
@@ -1,11 +1,16 @@
+import ProjectReportService from "@/app/lib/services/ProjectReportService";
 import { Button } from "@/components/ui/button";
 import { Dialog, DialogDescription, DialogFooter, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
-import { useTranslations } from "next-intl";
+import { useFormatter, useTranslations } from "next-intl";
 import { useState } from "react";
 
-
 const ViewDialog = ({ data, onClose, isOpen }) => {
+    const t = useTranslations('ProjectReportPage');
+    const format = useFormatter();
+
+    let projectReport = data?.data;
+
     return (
         <Dialog onOpenChange={onClose} open={isOpen} modal defaultOpen={isOpen}>
             <DialogContent>
@@ -15,8 +20,30 @@ const ViewDialog = ({ data, onClose, isOpen }) => {
                         View report from salesman
                     </DialogDescription>
                 </DialogHeader>
+                <div className="flex flex-col gap-3">
+                    <div className="mb-3">
+                        <label htmlFor="salesman_name" className="block mb-2 font-medium">{t('salesman_name')}</label>
+                        <span id="salesman_name" className="italic">{projectReport?.salesman_name}</span>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="salesman_name" className="block mb-2 font-medium">{t('created_at')}</label>
+                        <span id="salesman_name" className="italic">{projectReport && projectReport.created_at ? format.dateTime(new Date(projectReport?.created_at), {
+                            year: 'numeric',
+                            month: 'short',
+                            day: 'numeric',
+                        }) : ''}</span>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="title" className="block mb-2 font-medium">{t('report_title')}</label>
+                        <span id="title" className="italic">{projectReport?.title}</span>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="content" className="block mb-2 font-medium">{t('report_content')}</label>
+                        <span id="content" className="italic">{projectReport?.content}</span>
+                    </div>
+                </div>
                 <DialogFooter>
-                    <Button type="submit">Save changes</Button>
+                    <Button type="button" onClick={() => translate()}>Translate</Button>
                 </DialogFooter>
             </DialogContent>
         </Dialog>
@@ -29,14 +56,19 @@ const DataTable = (data, ...props) => {
     const projects = data.data;
 
     const [isDialogOpen, setDialogOpen] = useState(false);
+    const [singleDataset, setSingleDataset] = useState(null);
 
-    const viewDataset = async () => {
+    const viewDataset = async (id) => {
+        const response = await ProjectReportService.fetchProjectReport(id);
+
+        setSingleDataset(response);
+
         setDialogOpen(true);
     }
 
     return (
         <>
-            <ViewDialog onClose={setDialogOpen} isOpen={isDialogOpen} />
+            <ViewDialog onClose={setDialogOpen} isOpen={isDialogOpen} data={singleDataset} />
 
             <div className="bg-white rounded-md border">
                 <Table>
@@ -60,7 +92,7 @@ const DataTable = (data, ...props) => {
                                         <TableCell className="font-base">{project.content}</TableCell>
                                         <TableCell className="font-base">{project.created_at}</TableCell>
                                         <TableCell>
-                                            <Button onClick={() => viewDataset()}>View</Button>
+                                            <Button onClick={() => viewDataset(project.id)}>View</Button>
                                         </TableCell>
                                     </TableRow>
                                 ))}
--- app/lib/services/ProjectReportService.js
+++ app/lib/services/ProjectReportService.js
@@ -24,6 +24,28 @@ class ProjectReportService {
             return { success: false, message: 'Something went wrong. Please, try again later' };
         }
     }
+
+    fetchProjectReport = async (id) => {
+        try {
+            const response = await fetch(`${this.endpoints}/api/v1/project-report/${id}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                }
+            });
+
+            const data = await response.json();
+
+            if (!response.ok) return Object.assign({}, { success: false }, data);
+
+            return Object.assign({}, { success: false }, data);
+        } catch (error) {
+            console.error(error);
+
+            return { success: false, message: 'Something went wrong. Please, try again later' };
+        }
+    }
 }
 
 export default new ProjectReportService();
